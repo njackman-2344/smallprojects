750d4cf3c822bf3287f5ea0596df853975dde68e
njackman-2344 <110741503+njackman-2344@users.noreply.github.com>
2024-05-01 15:47:09 -0700

    character-name-generator
    simple program
    
    start of 100days of code

---
+++ char-name-gen.py
@@ -0,0 +1,13 @@
+#Character Name Generator
+
+
+def character_name_generator():
+  print("Welcome to the Character Name Generator!")
+
+  first = input("What was your crush's first name?: ")
+
+  last = input("What is your favorite color?: ")
+  return print("Your character name is", first, last)
+
+
+character_name_generator()
